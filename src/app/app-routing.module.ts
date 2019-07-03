1d95bb546ec750eecfc5db07e415acf438764ea6
Szymon Rybka <artifexet@gmail.com>
2019-07-03 18:42:17 +0200

    added router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeViewComponent} from "./views/home-view/home-view.component";
+import {DetailsViewComponent} from "./views/details-view/details-view.component";
+import {FormViewComponent} from "./views/form-view/form-view.component";
+import {TermsViewComponent} from "./views/terms-view/terms-view.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeViewComponent
+  },
+  {
+    path: 'view',
+    component: DetailsViewComponent,
+  },
+  {
+    path: 'add',
+    component: FormViewComponent,
+  },
+  {
+    path: 'terms',
+    component: TermsViewComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './layout/layout.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { TermsViewComponent } from './views/terms-view/terms-view.component';
+import { FormViewComponent } from './views/form-view/form-view.component';
+import { DetailsViewComponent } from './views/details-view/details-view.component';
+import { HomeViewComponent } from './views/home-view/home-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    TermsViewComponent,
+    FormViewComponent,
+    DetailsViewComponent,
+    HomeViewComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
