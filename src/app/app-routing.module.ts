4502c3212def9d5c50927d2f01f3765ccbc4278e
Szymon Rybka <artifexet@gmail.com>
2019-07-16 22:36:54 +0200

    Fixing lint errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HomeViewComponent} from "./views/home-view/home-view.component";
-import {DetailsViewComponent} from "./views/details-view/details-view.component";
-import {FormViewComponent} from "./views/form-view/form-view.component";
-import {TermsViewComponent} from "./views/terms-view/terms-view.component";
+import {HomeViewComponent} from './views/home-view/home-view.component';
+import {DetailsViewComponent} from './views/details-view/details-view.component';
+import {FormViewComponent} from './views/form-view/form-view.component';
+import {TermsViewComponent} from './views/terms-view/terms-view.component';
 
 
 const routes: Routes = [
