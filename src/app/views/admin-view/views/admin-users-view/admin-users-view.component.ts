a2d2f101c28a24daed64e1acd2bce78185c02fec
Szymon Rybka <artifexet@gmail.com>
2019-08-15 23:36:36 +0200

    Added sample admin users page,

--- src/app/views/admin-view/views/admin-users-view/admin-users-view.component.ts
+++ src/app/views/admin-view/views/admin-users-view/admin-users-view.component.ts
@@ -1,22 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFireFunctions} from "@angular/fire/functions";
+import {UserInfo} from "../../../../services/authentication/user-info";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-admin-user-view',
   templateUrl: './admin-users-view.component.html',
-  styleUrls: ['./admin-users-view.component.scss']
+  styleUrls: ['./admin-users-view.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class AdminUsersViewComponent implements OnInit {
+  userList: UserInfo[];
+  columnsToDisplay = ['name', 'email', 'admin'];
+  expandedElement: UserInfo | null;
 
   constructor(private fns: AngularFireFunctions) { }
 
   ngOnInit() {
+    this.updateUsers();
   }
 
-  add(email) {
+  private updateUsers(): void {
+    const callable = this.fns.httpsCallable('getUsers');
+    callable({}).subscribe(value => {
+      this.userList = value.result;
+    })
+  }
+
+  setAsAdmin(uid: string) {
     const callable = this.fns.httpsCallable('setAsAdmin');
-    callable({ email }).subscribe(value => {
-      console.log(value)
+    callable({ uid }).subscribe(value => {
+      this.updateUsers();
     })
   }
 
